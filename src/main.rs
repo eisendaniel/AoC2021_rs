fd31a6fa477931c7125e2faf01f6c5451b0ed1cd
eisendaniel <danieleisen99@gmail.com>
2021-12-06 07:52:23 +1300

    day 2p2

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,52 @@ fn day_two() -> u32 {
     depth * h_pos
 }
 
+fn day_two_two() -> i32 {
+    let input: Vec<&str> = include_str!("../inputs/daytwo.txt").lines().collect();
+
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut h_pos = 0;
+
+    for command in input {
+        let mut split = command.split_whitespace();
+        match split.next() {
+            Some("forward") => {
+                let x = split.next().unwrap().parse::<i32>().unwrap();
+                h_pos += x;
+                depth += aim * x;
+            }
+            Some("down") => aim += split.next().unwrap().parse::<i32>().unwrap(),
+            Some("up") => aim -= split.next().unwrap().parse::<i32>().unwrap(),
+            _ => panic!("UNKNOWN TOKEN"),
+        }
+    }
+
+    depth * h_pos
+}
+
+fn day_three() -> String {
+    String::from("TODO")
+}
+
+fn day_four() -> String {
+    String::from("TODO")
+}
+
+fn day_five() -> String {
+    String::from("TODO")
+}
+
+fn day_six() -> String {
+    String::from("TODO")
+}
+
 fn main() {
     // println!("Day 1: {}", day_one());
-    println!("Day 2: {}", day_two());
+    // println!("Day 2: {}", day_two());
+    // println!("Day 2 part 2: {}", day_two_two());
+    println!("Day 3: {}", day_three());
+    println!("Day 4: {}", day_four());
+    println!("Day 5: {}", day_five());
+    println!("Day 6: {}", day_six());
 }
